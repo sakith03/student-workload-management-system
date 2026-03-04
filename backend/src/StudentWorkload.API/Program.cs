5aa374e8d712a8bf1ad7c3aac4544f4f23ffbd7e
Riviya <rivindu.perera3@gmail.com>
2026-03-04 23:55:56 +0530

    updated program.cs

--- backend/src/StudentWorkload.API/Program.cs
+++ backend/src/StudentWorkload.API/Program.cs
@@ -78,7 +78,7 @@ builder.Services.AddCors(options =>
         policy.WithOrigins(
             "http://localhost:5173",
             "http://localhost:3000",
-            "https://frontend-loadmate.azurewebsites.net"  // ← add this
+            "https://frontend-loadmate-h5h2gghtascvcnay.centralindia-01.azurewebsites.net" 
         )
         .AllowAnyHeader()
         .AllowAnyMethod());
