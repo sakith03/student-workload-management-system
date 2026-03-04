f7ef38f240b8e1d93769d884a4d72ec0c1355d0e
Riviya <rivindu.perera3@gmail.com>
2026-03-04 22:40:20 +0530

    modified axiousConfig

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const apiUrl = import.meta.env.VITE_API_URL
   ? `${import.meta.env.VITE_API_URL}/api`
-  : 'http://localhost:5000/api';
+  : '/api';
 
 const api = axios.create({
   baseURL: apiUrl,
